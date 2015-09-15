815858293dee98649c926d904309e75593259efe
Scott Williams <scott.joe.williams@gmail.com>
2015-09-14 20:01:35 -0400

    styling fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,9 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 	  sass: {
 	    dist: {
+	      options: {
+	      	style: 'compressed'
+	      },
 	      files: {
 	        './public/css/main.css': './public/sass/main.scss'
 	      }
